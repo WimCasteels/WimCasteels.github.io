831487d7b2d056b4010ea44bed6804ded3ba6990
WimCasteels <67267294+WimCasteels@users.noreply.github.com>
2025-01-04 20:02:18 +0000

    Deploying to gh-pages from @ WimCasteels/WimCasteels.github.io@e1dbf921295d777a3e5c2894341a0a67fd7c195e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,33 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "🔨Wordt aan gewerkt🔨",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-lezingen",
           title: "lezingen",
-          description: "Informatie over aankomende lezingen en slides van voorbije lezingen.",
+          description: "Informatie over aankomende en voorbije lezingen.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/lezingen/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
           },
         },{id: "post-a-post-with-image-galleries",
       
