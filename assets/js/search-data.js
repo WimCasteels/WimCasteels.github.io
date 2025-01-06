c047e0014742008a6841b2e33591e92b250b5cda
WimCasteels <67267294+WimCasteels@users.noreply.github.com>
2025-01-06 22:55:10 +0000

    Deploying to gh-pages from @ WimCasteels/WimCasteels.github.io@59a20f1181663b1881f44c9f58ee2b9e3d0d7d5a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -360,16 +360,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-de-eerste-aflevering-van-de-genius-podcast-staat-online-intelligent-omgaan-met-artificiële-intelligentie-met-caroline-van-cauwelaert-ceo-van-epcon-en-wim-casteels",
+          title: 'De eerste aflevering van de Genius Podcast staat online: Intelligent omgaan met artificiële...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
